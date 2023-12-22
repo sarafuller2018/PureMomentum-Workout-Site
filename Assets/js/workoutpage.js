5bd12caf3dbe1ef0f87eef221e123e3502377c55
Bernardo Alonso <bernardo4430@gmail.com>
2023-12-21 21:15:58 -0600

    added work out api X-RapidAPI-Key

--- Assets/js/workoutpage.js
+++ Assets/js/workoutpage.js
@@ -1,4 +1,20 @@
 // API code for getting workouts information
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=abdominals',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '837bd24d63mshff4c202742d0dbfp14a012jsn2368043f393c',
+		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
 var muscle = 'biceps'
 $.ajax({
     method: 'GET',

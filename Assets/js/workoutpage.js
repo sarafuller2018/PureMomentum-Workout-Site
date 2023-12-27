baf69b80fc0bb0bbd31c93cef99c228ce46046f0
Bernardo Alonso <bernardo4430@gmail.com>
2023-12-27 15:24:29 -0600

    added js for work out timer and some css for replacing color when workout not available

--- Assets/js/workoutpage.js
+++ Assets/js/workoutpage.js
@@ -1,30 +1,65 @@
-// API code for getting workouts information
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=abdominals',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '837bd24d63mshff4c202742d0dbfp14a012jsn2368043f393c',
-		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// get the from button and save it to local storage
+var workoutLinks = document.querySelectorAll(".title-names");
+var workoutTitle = localStorage.getItem("title");
 
 
-var muscle = 'biceps'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
+
+// 0.1 set the text value of the h1 by pulling from local storage 
+var workoutNameElement = document.getElementById("workout-name");
+if (workoutNameElement) {
+    workoutNameElement.innerText = workoutTitle ;
+}
+
+
+//0.2 to check if workout title exist in local storage
+function checkWorkoutTitleInLocalStorage(workoutTitle) {
+    return localStorage.getItem(workoutTitle) !==null;
+}
+
+//0.3 display the date on screen
+function displayFinishRestingDay(workoutTitle){
+    // get value from local storage using the workout title as the key
+    var restingDayValue = localStorage.getItem(workoutTitle);
+
+    // display the value in finish-resting-day element
+    var FinishRestingDayElement = document.getElementById("finish-resting-day");
+    if (FinishRestingDayElement) {
+        FinishRestingDayElement.innerText = restingDayValue;
+    }
+}
+
+//0.4 finish button adds 7 day to current date and saves to local storage and naming it aftet what the title text is 
+document.getElementById("finish-button").addEventListener("click",function(){
+    
+    if (workoutTitle) {
+
+        // get current date
+        var currentDate = new Date();
+
+        // add 1 day to the current date
+        var sevenDaysLater = new Date(currentDate.getTime()+ (3 * 24 * 60 * 60 * 1000));
+
+        //saves the calculated date to local storage with workout as  the key
+        formattedDate = sevenDaysLater.toLocaleDateString()
+        localStorage.setItem(workoutTitle, formattedDate);
+
+
+        // displays the date in consil 
+        console.log( formattedDate + " until " + workoutTitle + " is available");
+
+        // check if workout title exists in local storage and if it does display value
+        if (checkWorkoutTitleInLocalStorage(workoutTitle)) {
+            displayFinishRestingDay(workoutTitle);
+
+        }
+    } else {
+        alert('No workout found ')
     }
 });
+
+
+//0.5 check if workout exist in local storage when page loads
+var workoutTitleOnLoad =localStorage.getItem('title');
+if (workoutTitleOnLoad && checkWorkoutTitleInLocalStorage(workoutTitleOnLoad)) {
+    displayFinishRestingDay(workoutTitleOnLoad);
+}

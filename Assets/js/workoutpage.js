392242dc026e53c44c34dafd24f09aeefe271548
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-01-04 21:39:08 -0600

    added colors to logic on AQI

--- Assets/js/workoutpage.js
+++ Assets/js/workoutpage.js
@@ -1,138 +1,125 @@
-// get the from button and save it to local storage
+// Get the from button and save it to local storage
 var workoutLinks = document.querySelectorAll(".title-names");
 var workoutTitle = localStorage.getItem("title");
 
-
-
-// 0.1 set the text value of the h1 by pulling from local storage 
+// 0.1 Set the text value of the h1 by pulling from local storage 
 var workoutNameElement = document.getElementById("workout-name");
 if (workoutNameElement) {
-    workoutNameElement.innerText = workoutTitle ;
+    workoutNameElement.innerText = workoutTitle;
 }
 
-
-//0.2 to check if workout title exist in local storage
+//0.2 To check if workout title exist in local storage
 function checkWorkoutTitleInLocalStorage(workoutTitle) {
-    return localStorage.getItem(workoutTitle) !==null;
+    return localStorage.getItem(workoutTitle) !== null;
 }
 
-//0.3 display the date on screen
-function displayFinishRestingDay(workoutTitle){
+//0.3 Display the date on screen
+function displayFinishRestingDay(workoutTitle) {
     // get value from local storage using the workout title as the key
     var restingDayValue = localStorage.getItem(workoutTitle);
 
     // display the value in finish-resting-day element
     var FinishRestingDayElement = document.getElementById("finish-resting-day");
     if (FinishRestingDayElement) {
-        FinishRestingDayElement.innerText = "Exercise will be available on : "+ restingDayValue;
+        FinishRestingDayElement.innerText = "Exercise will be available on : " + restingDayValue;
     }
 }
 
-//0.4 finish button adds 7 day to current date and saves to local storage and naming it aftet what the title text is 
-document.getElementById("finish-button").addEventListener("click",function(){
-    
+//0.4 Finish button adds 7 day to current date and saves to local storage and naming it aftet what the title text is 
+document.getElementById("finish-button").addEventListener("click", function () {
+
     if (workoutTitle) {
 
-        // get current date
+        // Get current date
         var currentDate = new Date();
 
-        // add 1 day to the current date
-        var sevenDaysLater = new Date(currentDate.getTime()+ (3 * 24 * 60 * 60 * 1000));
+        // Add 1 day to the current date
+        var sevenDaysLater = new Date(currentDate.getTime() + (3 * 24 * 60 * 60 * 1000));
 
-        //saves the calculated date to local storage with workout as  the key
+        // Saves the calculated date to local storage with workout as  the key
         formattedDate = sevenDaysLater.toLocaleDateString()
         localStorage.setItem(workoutTitle, formattedDate);
 
+        // Displays the date in console
+        console.log(formattedDate + " until " + workoutTitle + " is available");
 
-        // displays the date in consil 
-        console.log( formattedDate + " until " + workoutTitle + " is available");
-
-        // check if workout title exists in local storage and if it does display value
+        // Check if workout title exists in local storage and if it does display value
         if (checkWorkoutTitleInLocalStorage(workoutTitle)) {
             displayFinishRestingDay(workoutTitle);
 
+        } else {
+            alert('No workout found ')
         }
-    } else {
-        alert('No workout found ')
     }
 });
 
-
-//0.5 check if workout exist in local storage when page loads
-var workoutTitleOnLoad =localStorage.getItem('title');
+//0.5 Check if workout exist in local storage when page loads
+var workoutTitleOnLoad = localStorage.getItem('title');
 if (workoutTitleOnLoad && checkWorkoutTitleInLocalStorage(workoutTitleOnLoad)) {
     displayFinishRestingDay(workoutTitleOnLoad);
 }
 
+//------- exercises API ----------//
 
-//-------exercises API ----------//
-
-// getting the title text as the exercise name 
+// Getting the title text as the exercise name 
 var muscle = workoutTitle
 const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?' + muscle,
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '837bd24d63mshff4c202742d0dbfp14a012jsn2368043f393c',
-		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-	}
+    async: true,
+    crossDomain: true,
+    url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?' + muscle,
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '837bd24d63mshff4c202742d0dbfp14a012jsn2368043f393c',
+        'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+    }
 };
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
-    // checks if the response is an array and has at least 5 items
+    console.log(response);
+    // Checks if the response is an array and has at least 5 items
     if (Array.isArray(response) && response.length >= 5) {
-        //makes the array a random order 
+        // Makes the array a random order 
         const shuffleExercises = response.sort(() => Math.random() - 0.5);
-        // get the first 5 items from the array
+        // Get the first 5 items from the array
         const randomExercises = shuffleExercises.slice(0, 5);
 
-        // get id element from html
+        // Get id element from html
         const container = $("#exerciseContainer");
 
-        // clears container
+        // Clears container
         container.empty();
 
-        // loop through exercises and append them to the container
+        // Loop through exercises and append them to the container
         randomExercises.forEach((exercise, index) => {
-            // create a unique ID for each div
+            // Create a unique ID for each div
             const divID = "exerciseDiv" + index;
 
-            //check if theres a timer on this page and if there is dont show exercises
+            // Check if theres a timer on this page and if there is dont show exercises
             if (!checkWorkoutTitleInLocalStorage(workoutTitle)) {
-               const exerciseDiv = $("<div>").attr("id", divID).addClass("exercise-styling"); 
+                const exerciseDiv = $("<div>").attr("id", divID).addClass("exercise-styling");
 
-                // create element 
-                const exerciseName = $("<h1>").attr('id',divID).text("Exercise: " + exercise.name);
-                const exercisedifficulty = $("<p>").attr('id',divID).text("Difficulty: " + exercise.difficulty);
-                const exerciseequipment = $("<p>").attr('id',divID).text("Equipment: " + exercise.equipment);
-                const exerciseinstructions= $("<p>").attr('id',divID).text("Instructions: " + exercise.instructions);
-                const exercisemuscle= $("<p>").attr('id',divID).text("Muscle: " + exercise.muscle);
-                const exercisetype= $("<p>").attr('id',divID).text("Type: " + exercise.type);
-                
-                // appends each element inside a div 
-                exerciseDiv.append (exerciseName);
-                exerciseDiv.append (exercisemuscle);
-                exerciseDiv.append (exercisetype);
-                exerciseDiv.append (exercisedifficulty);
-                exerciseDiv.append (exerciseequipment);
-                exerciseDiv.append (exerciseinstructions);
+                // Create element 
+                const exerciseName = $("<h1>").attr('id', divID).text("Exercise: " + exercise.name);
+                const exercisedifficulty = $("<p>").attr('id', divID).text("Difficulty: " + exercise.difficulty);
+                const exerciseequipment = $("<p>").attr('id', divID).text("Equipment: " + exercise.equipment);
+                const exerciseinstructions = $("<p>").attr('id', divID).text("Instructions: " + exercise.instructions);
+                const exercisemuscle = $("<p>").attr('id', divID).text("Muscle: " + exercise.muscle);
+                const exercisetype = $("<p>").attr('id', divID).text("Type: " + exercise.type);
 
+                // Appends each element inside a div 
+                exerciseDiv.append(exerciseName);
+                exerciseDiv.append(exercisemuscle);
+                exerciseDiv.append(exercisetype);
+                exerciseDiv.append(exercisedifficulty);
+                exerciseDiv.append(exerciseequipment);
+                exerciseDiv.append(exerciseinstructions);
 
-                // appends all the divs inside the container
+                // Appends all the divs inside the container
                 container.append(exerciseDiv);
-            }else {
+            } else {
                 null
             }
-            
-
-
-            
-
         })
-
 
         console.log(randomExercises);
     }

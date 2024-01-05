eaeec491d831e6049180f73bfa5ec16958deb355
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-01-04 20:24:07 -0600

    added journals to local storage and appended to page

--- Assets/js/journallog.js
+++ Assets/js/journallog.js
@@ -1,2 +1,37 @@
-//hey guys
+var journalEntryEl = document.getElementById("journalEntry");
+var journalEntrySubmitButtonEl = document.getElementById("submitJournalButton");
+var currentJournalEl = document.getElementById("currentJournal");
+var previousJournalsEl = document.getElementById("previousJournals");
+var savedJournalArray = [];
 
+if (JSON.parse(localStorage.getItem("journals")) !== null) {
+    savedJournalArray = JSON.parse(localStorage.getItem("journals"));
+};
+
+journalEntrySubmitButtonEl.addEventListener("click", function () {
+
+    var journalEntry = journalEntryEl.value;
+    console.log(journalEntry);
+
+    var addCurrentJournalEl = document.createElement("p");
+    addCurrentJournalEl.textContent = journalEntry;
+    currentJournalEl.appendChild(addCurrentJournalEl);
+
+    savedJournalArray.unshift(journalEntry)
+    console.log(savedJournalArray);
+
+    localStorage.setItem("journals", JSON.stringify(savedJournalArray));
+});
+
+function renderHistory() {
+    var journalArray = JSON.parse(localStorage.getItem("journals"));
+    console.log(journalArray);
+
+    for (i=0; i<journalArray.length; i++) {
+        var addJournalEl = document.createElement("p");
+        addJournalEl.textContent = journalArray[i];
+        previousJournalsEl.appendChild(addJournalEl);
+    }
+};
+
+renderHistory();
